9834bc0095d7a54c6d9238b41ed3351e0641106e
Skaistev <153711410+Skaistev@users.noreply.github.com>
2024-03-13 23:23:15 +0200

    time-to-text

--- api/index.js
+++ api/index.js
@@ -76,22 +76,49 @@ apiRouter.get ('/api/time-as-text', (req, res)=> {
     const min = time.getMinutes(); 
     const sec = time.getSeconds(); 
 
+    const valString = val.toString();
+    const minString = min.toString();
+    const secString = sec.toString();
+
     const vienetai = [
-        '',"Viena","Dvi", "Trys","Keturios","Penkios",'Šešios','Septynios','Astuonios','Devynios','Desimt'
+        '',"viena","dvi", "trys","keturios","penkios",'šešios','septynios','astuonios','devynios','desimt'
     ]
     const desimtys = [
          '', "vienuolika",'dvylika','trylika','keturiolika','penkiolika','sesiolika','septyniolika','astuoniolika','devyniolika'
     ]
     const simtai = [
-        '','dvidešimt','trisdešimt','keturiasdešimt','penkiasdėšimt'
+        '','dvidešimt','trisdešimt','keturiasdešimt','penkiasdešimt'
     ]
+    
+
+    let valandos = '';
+    if(val<1){valandos='Nulis'}
+    if(val<11&&val>0){valandos=vienetai[val]}
+    if(val>10&&val<20){valandos=desimtys[val-10]}
+    if(val>19){valandos=simtai[valString[0]-1] +' '+ vienetai[valString[1]]}
+
+    let minutes = '';
+    
+    if(min<11){minutes=vienetai[min]}
+    if(min>10&&min<20){minutes=desimtys[min-10]}
+    if(min>19){minutes=simtai[minString[0]-1] +' '+ vienetai[minString[1]]}
+
+    let sekundes = '';
+ 
+    if(sec<11){sekundes=vienetai[sec]}
+    if(sec>10&&sec<20){sekundes=desimtys[sec-10]}
+    if(sec>19){sekundes=simtai[secString[0]-1] +' '+ vienetai[secString[1]]}
+
+    // let valandosZodis = ''=1?valandosZodis="valanda":
+    // val<9&&val>1?valandosZodis="valandos":'valandŲ';
+    
+    
+   console.log(sec);
+   res.send (`<h1>${valandos} valanda ${min===0?'': minutes + " minutes"} ${sec===0?'': sekundes + " sekundes"}</h1>`)
+})
 
 
 
-
-
-    res.send (`<h1></h1>`)
-    })
 
 
 export { apiRouter }
